a09232188ba9846ac582a431073fe9a6e7d3adb9
defendimattia <defendi.mattia92@gmail.com>
2025-03-26 15:08:45 +0100

    add: "/bacheca" route

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const postList = require('./data/post-array.js')
+
+
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
+})
+
+app.get('/bacheca', (req, res) => {
+    res.json(postList)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
 
